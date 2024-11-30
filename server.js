cbe13d3edf31e7b531c538adda36a353559f7677
Ephraim Arizona <degreatarizona@gmail.com>
2024-11-30 09:01:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,19 @@ app.use(cors({
 // Handle preflight requests globally
 app.options("*", cors());
 
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT, DELETE');
+  res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Origin, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+  );
+  next();
+});
+
+
 // Parse incoming requests with JSON payloads
 app.use(express.json());
 
